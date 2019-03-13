6acf8657d60d9077f07cdbda6b1cb7b5fc3d6633
HerisHernandez <ultra_javier007@hotmail.com>
2019-03-12 18:30:16 -0600

    Sincro

--- Binarios/ResBinario.BL/ProductosBL.cs
+++ Binarios/ResBinario.BL/ProductosBL.cs
@@ -9,20 +9,55 @@ namespace ResBinario.BL
     public class ProductosBL
     {
         Contexto _contexto;
-        public List<Producto> ListaProductos { get; set; }
+        public List<Producto> ListadeProductos { get; set; }
 
         public ProductosBL()
         {
             _contexto = new Contexto();
-            ListaProductos = new List<Producto>();
+            ListadeProductos = new List<Producto>();
 
         }
         public List<Producto> ObtenerProductos()
         {
 
-            ListaProductos = _contexto.Productos.ToList();
-            return ListaProductos;
+            ListadeProductos = _contexto.Productos
+                .Include("Categoria")
+                .ToList();
 
+            return ListadeProductos;
+
+        }
+
+        public void GuardarProducto(Producto producto)
+        {
+            if(producto.Id == 0)
+            {
+                _contexto.Productos.Add(producto);
+            }else
+            {
+                var productoExistente = _contexto.Productos.Find(producto.Id);
+                productoExistente.Descripcion = producto.Descripcion;
+                productoExistente.Precio = producto.Precio;
+                productoExistente.UrlImagen = producto.UrlImagen;
+            }
+
+            _contexto.SaveChanges();
+        }
+
+        public Producto ObtenerProducto(int id)
+        {
+            var producto = _contexto.Productos
+                .Include("Categoria").FirstOrDefault(p => p.Id == id);
+
+            return producto;
+        }
+
+        public void EliminarProducto(int id)
+        {
+            var producto = _contexto.Productos.Find(id);
+
+            _contexto.Productos.Remove(producto);
+            _contexto.SaveChanges();
         }
     }
 }
